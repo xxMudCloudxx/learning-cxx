6ebf22e73ac9cbab3a3e896dab8b872d8d86e98c
xxbaizero <xxbaizero@gmail.com>
2024-07-31 17:45:30 +0800

    done-20-21-25

--- exercises/20_class_template/main.cpp
+++ exercises/20_class_template/main.cpp
@@ -10,32 +10,48 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         std::copy(shape_, shape_ + 4, shape);
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
-         for (unsigned int i = 0; i < 4; ++i) {
+        for (unsigned int i = 0; i < 4; ++i) {
             size *= shape[i];
         }
         data = new T[size];
-        std::memcmp(data, data_, size * sizeof(T));
+        std::memcpy(data, data_, size * sizeof(T));
     }
+
     ~Tensor4D() {
         delete[] data;
     }
 
-    // 为了保持简单，禁止复制和移动
     Tensor4D(Tensor4D const &) = delete;
     Tensor4D(Tensor4D &&) noexcept = delete;
 
-    // 这个加法需要支持“单向广播”。
-    // 具体来说，`others` 可以具有与 `this` 不同的形状，形状不同的维度长度必须为 1。
-    // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
-    // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
-    // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
-        // TODO: 实现单向广播的加法
-        
+        unsigned int this_size = 1;
+        for (unsigned int i = 0; i < 4; ++i) {
+            this_size *= shape[i];
+        }
+
+        for (unsigned int i = 0; i < this_size; ++i) {
+            unsigned int idx[4];
+            unsigned int temp = i;
+            for (int j = 3; j >= 0; --j) {
+                idx[j] = temp % shape[j];
+                temp /= shape[j];
+            }
+
+            unsigned int other_idx = 0;
+            for (int j = 0; j < 4; ++j) {
+                other_idx *= others.shape[j] == 1 ? 1 : shape[j];
+                other_idx += (others.shape[j] == 1) ? 0 : idx[j];
+            }
+
+            data[i] += others.data[other_idx];
+        }
+
         return *this;
     }
 };
+
+
 
 // ---- 不要修改以下代码 ----
 int main(int argc, char **argv) {
@@ -107,8 +123,8 @@ int main(int argc, char **argv) {
         auto t0 = Tensor4D(s0, d0);
         auto t1 = Tensor4D(s1, d1);
         t0 += t1;
-        for (unsigned int i = 0; i < sizeof(d0) / sizeof(int); i++) {
-            ASSERT(t0.data[i] == t0.data[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
+        for (unsigned int i = 0; i < sizeof(d0) / sizeof(double); i++) {
+            ASSERT(t0.data[i] == d0[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
         }
     }
 }
--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -9,9 +9,12 @@ struct Tensor {
 
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
+        for (unsigned int i = 0; i < N; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
-        std::memcmp(data, 0, size * sizeof(T));
+        std::memset(data, 0, size * sizeof(T)); 
     }
     ~Tensor() {
         delete[] data;
@@ -31,10 +34,12 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
-        for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i]);
-            // TODO: 计算 index
+        unsigned int multiplier = 1;
+        for (int i = N - 1; i >= 0; --i) {
+            index += indices[i] * multiplier;
+            multiplier *= shape[i];
         }
+        return index;
     }
 };
 
